764bfaf2f53bf777af7fe1c0584814ce352bb6f2
Nurdin Ismail <shiilnurdin@gmail.com>
2024-04-25 18:58:34 +0300

    Finished account information section

--- client/src/components/user/User.jsx
+++ client/src/components/user/User.jsx
@@ -6,10 +6,75 @@ export default function User({user}) {
   const [current, setcurrent] = useState('Account Information')
 
   function handleButtonChange(target){
-    if(target == 'Account Information'){
-      return <h1>
-        Account Information
-      </h1>
+    if(target == 'Account Information' && user){
+      return <div className=' mx-6  bg-gray-100 grid h-[490px] grid-rows-[10%_40%_40%]'>
+
+        <h1 className=' text-4xl font-[200]'>Account Information</h1>
+
+        <div className=' grid grid-cols-2 border-black border-b-[1px]'>
+
+          <div className='flex flex-col center-start'>
+
+            <h1 className=' font-semibold text-xl'>Contact Information</h1>
+            <h1 className=' text-[17px]'>{user.name}</h1>
+            <h1>{user.email.replace('example.net', 'gmail.com')}</h1>
+            <h1 className='  text-sky-600 cursor-pointer select-none w-9' onClick={() => setcurrent('Edit Information')}>Edit</h1>
+
+
+
+            
+
+          </div>
+
+          <div className='flex flex-col center-start'>
+
+            <h1 className='font-semibold text-xl'>Newsletter</h1>
+            <p>You aren't subscribed to our newsletter</p>
+            <h1 className='  text-sky-600 cursor-pointer select-none w-9' onClick={() => setcurrent('Edit Information')}>Edit</h1>
+
+
+          </div>
+
+        </div>
+        <div className=' grid-rows-2'>
+
+          <div className='my-5 flex items-center justify-between'>
+                      <h1 className=' text-4xl font-[200]  '>Address Book</h1>
+                      <h1 className='  text-sky-600 cursor-pointer select-none  w-24' onClick={() => setcurrent('Adress')}>View all</h1>
+
+          </div>
+
+
+          <div className='grid grid-cols-2'>
+            <div className='flex flex-col center-start'>
+
+              <h1 className=' font-semibold text-xl'>Default Billing Adress</h1>
+
+              <p>You have not set a default billing adress</p>
+              <h1 className='  text-sky-600 cursor-pointer select-none  w-24' onClick={() => setcurrent('Adress')}>Edit Adress</h1>
+
+
+
+            
+
+          </div>
+
+          <div className='flex flex-col center-start'>
+
+            <h1 className=' font-semibold text-xl'>Default Shipping Adress</h1>
+
+              <p>You have not set a default shipping adress</p>
+              <h1 className='  text-sky-600 cursor-pointer select-none  w-24' onClick={() => setcurrent('Adress')}>Edit Adress</h1>
+
+
+
+
+          </div>
+          </div>
+
+        </div>
+
+      </div>
 
     }else if(target == 'Adress'){
       return <h1>
@@ -42,7 +107,7 @@ export default function User({user}) {
         <div className=' grid item-center-end'>
 
           <div className='  h-[70%] w-[50%] sticky top-4 overflow-x-hidden '>
-            <ul className=' grid gap-2 '>
+            <ul className=' grid gap-2 select-none'>
               <li className='grid grid-cols-[20%_80%] smooth'
               style={current == 'Account Information' ? {transform: `translate(-${0}%)`} : {transform: `translate(-${20}%)`}}
               
@@ -130,10 +195,10 @@ export default function User({user}) {
 
         </div>
 
-        <div className='  grid item-center-start'>
+        <div className='  grid mt-[15vh]'>
 
 
-          <div className=' bg-gray-100 w-[90%] h-[70%]'>
+          <div className=' bg-gray-100 w-[90%] h-[60%]'>
             {handleButtonChange(current)}
 
 
